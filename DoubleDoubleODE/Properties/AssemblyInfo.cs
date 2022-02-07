b11c5e997a27d5612d45a2fc0861debbfbdc8ab4
tk-yoshimura <yoshimura8192@gmail.com>
2022-02-07 17:23:15 +0900

    + asm

---
+++ DoubleDoubleODE/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DoubleDouble.ODE")]
+[assembly: AssemblyDescription("double-double ode solver")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DoubleDouble.ODE")]
+[assembly: AssemblyCopyright("Copyright © T.Yoshimura 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("FE7AFD95-5A6A-48AE-8519-7D37ACFDC5EB")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
